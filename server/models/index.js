9d6b61e029ef7a607e464098b2ae724cd56197c9
Aaryavrat Arora <arc252000@gmail.com>
2023-10-12 13:09:03 +0530

    Modify DB settings

--- server/models/index.js
+++ server/models/index.js
@@ -15,6 +15,11 @@ let sequelize = new Sequelize(
     process.env.DB_PASSWORD, {
         dialect: process.env.DB_DIALECT,
         host: process.env.DB_HOST,
+        dialectOptions: {
+            ssl: {
+                require: true
+            }
+        },
 
     }
 
